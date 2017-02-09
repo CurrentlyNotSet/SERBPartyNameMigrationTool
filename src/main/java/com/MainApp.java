3c5ae24ca37c86378812ee490b1fd9c358d1e67e
altrdgenetics <tofudeliveryae86@hotmail.com>
2017-02-09 12:55:37 -0500

    Got around to properly using the MSSQL JDBC driver and adding an icon. Updated all of the dependancies while I was in there as well.

--- src/main/java/com/MainApp.java
+++ src/main/java/com/MainApp.java
@@ -6,6 +6,7 @@ import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -20,6 +21,8 @@ public class MainApp extends Application {
         controller.setDefaults(stage);
 
         Scene scene = new Scene(root);
+        stage.setTitle("Contact Name Migration Tool");
+        stage.getIcons().add(new Image("/image/1486683778_contacts.png"));
         stage.setScene(scene);
         stage.setResizable(false);
         stage.initStyle(StageStyle.UNDECORATED);
